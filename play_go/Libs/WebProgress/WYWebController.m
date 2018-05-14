a99c8c754f1bd8e47e50d0b286a685e164f9df17
xuwj <xuwj@wanbu.com.cn>
2018-05-14 14:08:02 +0800

    ------------1、缺失文件补充                2、xib文件在xcode9上无法运行问题解决

---
+++ play_go/Libs/WebProgress/WYWebController.m
@@ -0,0 +1,73 @@
+//
+//  WLWebController.m
+//  WangliBank
+//
+//  Created by 王启镰 on 16/6/21.
+//  Copyright © 2016年 iSoftstone infomation Technology (Group) Co.,Ltd. All rights reserved.
+//
+
+#import "WYWebController.h"
+#import "WYWebProgressLayer.h"
+#import "UIView+Frame.h"
+
+@interface WYWebController ()<UIWebViewDelegate>
+
+@end
+
+@implementation WYWebController
+{
+    UIWebView *_webView;
+    
+    WYWebProgressLayer *_progressLayer; ///< 网页加载进度条
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self setupUI];
+}
+
+
+- (void)setupUI {
+    _webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    _webView.delegate = self;
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:self.url]];
+    [_webView loadRequest:request];
+    _webView.opaque = NO;
+    _webView.backgroundColor = [UIColor whiteColor];
+   
+    [self.view addSubview:_webView];
+}
+
+
+#pragma mark - UIWebViewDelegate
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    [_progressLayer startLoad];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [self finishOrFailLoad];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    [self finishOrFailLoad];
+}
+
+- (void)finishOrFailLoad
+{
+    [_progressLayer finishedLoad];
+    [_progressLayer closeTimer];
+    [_progressLayer removeFromSuperlayer];
+    
+}
+-(void)viewWillAppear:(BOOL)animated
+{
+    _progressLayer = [[WYWebProgressLayer alloc]init];
+    _progressLayer.frame = CGRectMake(0, 42, SCREEN_WIDTH, 2);
+    
+    [self.navigationController.navigationBar.layer addSublayer:_progressLayer];
+
+}
+
+@end
